61adb138ce4f50d0b991094e814c3ac06476a41e
Tushar Choudhari <tushar@last9.io>
2024-07-09 13:56:29 +0530

    Add instrumentation code

--- javascript/with-react/src/main.tsx
+++ javascript/with-react/src/main.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import {
+  BatchSpanProcessor,
+  TracerConfig,
+  WebTracerProvider,
+} from "@opentelemetry/sdk-trace-web";
+import { ZoneContextManager } from "@opentelemetry/context-zone";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { Resource } from "@opentelemetry/resources";
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "react-app-example",
+  }),
+};
+
+const otlp = new OTLPTraceExporter({
+  url: import.meta.env.VITE_OTLP_ENDPOINT,
+  headers: {
+    Authorization: import.meta.env.VITE_OTLP_AUTH_HEADER,
+  },
+});
+
+const provider = new WebTracerProvider(providerConfig);
+
+// we will use ConsoleSpanExporter to check the generated spans in dev console
+provider.addSpanProcessor(new BatchSpanProcessor(otlp));
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [
+    getWebAutoInstrumentations({
+      "@opentelemetry/instrumentation-fetch": {},
+    }),
+  ],
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
