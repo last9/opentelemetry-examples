1f173d7f61e6033997d430567564da0aec5ef644
Tushar Choudhari <tushar@last9.io>
2024-07-09 11:19:08 +0530

    Add service name

--- javascript/with-express-ts/src/instrumentation.ts
+++ javascript/with-express-ts/src/instrumentation.ts
@@ -1,11 +1,22 @@
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import {
+  NodeTracerProvider,
+  TracerConfig,
+} from "@opentelemetry/sdk-trace-node";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"; // Use BatchSpanProcessor for better performance
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"; // Import OTLPTraceExporter
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { Resource } from "@opentelemetry/resources";
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "express-service",
+  }),
+};
 
 // Initialize and register the tracer provider
-const provider = new NodeTracerProvider();
+const provider = new NodeTracerProvider(providerConfig);
 const otlp = new OTLPTraceExporter({
   url: process.env.OTLP_ENDPOINT,
   headers: {
