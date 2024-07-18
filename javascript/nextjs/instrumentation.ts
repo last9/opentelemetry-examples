94417da636929779b736af79ce468b50b5b2577d
Tushar Choudhari <tushar@last9.io>
2024-07-18 15:55:36 +0530

    Add @vercel/otel

--- javascript/nextjs/instrumentation.ts
+++ javascript/nextjs/instrumentation.ts
@@ -1,25 +1,33 @@
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { registerOTel } from "@vercel/otel";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const provider = new NodeTracerProvider();
-    const otlp = new OTLPTraceExporter();
-
-    provider.addSpanProcessor(new BatchSpanProcessor(otlp));
-    provider.register();
-
-    registerInstrumentations({
-      instrumentations: [
-        getNodeAutoInstrumentations({
-          "@opentelemetry/instrumentation-fs": {
-            enabled: false,
-          },
-        }),
-      ],
-    });
+    registerOTel();
   }
 }
+
+// import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+// import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+// import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+// import { registerInstrumentations } from "@opentelemetry/instrumentation";
+// import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+// export async function register() {
+//   if (process.env.NEXT_RUNTIME === "nodejs") {
+//     const provider = new NodeTracerProvider();
+//     const otlp = new OTLPTraceExporter();
+
+//     provider.addSpanProcessor(new BatchSpanProcessor(otlp));
+//     provider.register();
+
+//     registerInstrumentations({
+//       instrumentations: [
+//         getNodeAutoInstrumentations({
+//           "@opentelemetry/instrumentation-fs": {
+//             enabled: false,
+//           },
+//         }),
+//       ],
+//     });
+//   }
+// }
