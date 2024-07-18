485c22ea322cc1f4b902502a589bdbdd4c27f300
Tushar Choudhari <tushar@last9.io>
2024-07-18 15:25:06 +0530

    Add nextjs example

---
+++ javascript/nextjs/instrumentation.ts
@@ -0,0 +1,25 @@
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const provider = new NodeTracerProvider();
+    const otlp = new OTLPTraceExporter();
+
+    provider.addSpanProcessor(new BatchSpanProcessor(otlp));
+    provider.register();
+
+    registerInstrumentations({
+      instrumentations: [
+        getNodeAutoInstrumentations({
+          "@opentelemetry/instrumentation-fs": {
+            enabled: false,
+          },
+        }),
+      ],
+    });
+  }
+}
