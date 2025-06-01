91595d82bbaabcb616d34466bde712b795ee15fa
Prathamesh Sonpatki <csonpatki@gmail.com>
2025-06-01 12:55:13 +0530

    Update code example for sidecar based otel collector

--- otel-collector/k8s-operator/app.py
+++ otel-collector/k8s-operator/app.py
@@ -2,8 +2,8 @@ import time
 import os
 
 def continuous_log_writer():
-    os.makedirs('/code', exist_ok=True)
-    with open('/code/app.log', 'a') as f:
+    os.makedirs('/log', exist_ok=True)
+    with open('/log/app.log', 'a') as f:
         while True:
             f.write(f"Log entry at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
             f.flush()
